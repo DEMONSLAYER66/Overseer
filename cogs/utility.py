2abf6a01e4b6eb436c088ffdd60ada2d35e7f5b9
XxJSweezeyxX <133728652+xxjsweezeyxx@users.noreply.github.com>
2023-07-21 04:00:08 -0500

    Update utility.py

--- cogs/utility.py
+++ cogs/utility.py
@@ -59,12 +59,8 @@ SERVER_ID = [1088118252200276071, 1117859916749742140]
 # Define the cooldown time (in seconds)
 cooldown_time = 7200  # 2 hours cooldown
 
-# Custom key function to create separate buckets for each guild
-def get_guild_key(ctx):
-    return ctx.guild.id
-
 # Create a cooldown mapping object for the /promote command
-promote_cooldown = commands.CooldownMapping.from_cooldown(1, cooldown_time, commands.BucketType.guild, key=get_guild_key)
+promote_cooldown = commands.CooldownMapping.from_cooldown(1, cooldown_time, commands.BucketType.guild)
 ####################### COOLDOWN FOR PROMOTION #########################
 
 
