97768938ae6f6f2e4afef36ce6050c44ec1dc7d8
XxJSweezeyxX <133728652+xxjsweezeyxx@users.noreply.github.com>
2023-07-22 18:33:12 -0500

    Update configuration.py

--- cogs/configuration.py
+++ cogs/configuration.py
@@ -590,11 +590,8 @@ class Configuration(commands.Cog):
                         await ctx.respond(f"Apologies {ctx.author.mention},\nAutopurged channels must have a *frequency* of at least `60s`.\n*Please try again.*", ephemeral=True)
                         return
             else:
-                if frequency == 0:
-                    pass
-                else:
-                    await ctx.respond(f"Apologies {ctx.author.mention},\nThe *frequency* parameter must be in the form `01d:01h:01m:01s` or a combination of this.\n*Please try again.*", ephemeral=True)
-                    return
+                await ctx.respond(f"Apologies {ctx.author.mention},\nThe *frequency* parameter must be in the form `01d:01h:01m:01s` or a combination of this.\n*Please try again.*", ephemeral=True)
+                return
     
           
             # Calculate the total frequency time in days, hours, minutes, and seconds
