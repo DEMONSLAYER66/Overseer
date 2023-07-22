56c300d3d6f2763ac8a27055a7ff9f87e4f749be
XxJSweezeyxX <133728652+xxjsweezeyxx@users.noreply.github.com>
2023-07-22 15:20:59 -0500

    Update utility.py

--- cogs/utility.py
+++ cogs/utility.py
@@ -396,11 +396,9 @@ class Utility(commands.Cog):
                 if remaining_time <= 0:
                     # Delete the cooldown time from MongoDB if the cooldown time is found and over
                     bump_db.cooldowns.delete_one(cooldown_data)
-                    continue
                 else:
                     # Save the remaining time to the database
                     bump_db.cooldowns.update_one({"_id": cooldown_data["_id"]}, {"$set": {"cooldown": remaining_time}})
-                    continue
 
 ############################# PROMOTE #########################
 
